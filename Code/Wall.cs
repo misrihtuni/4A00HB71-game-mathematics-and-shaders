83f4181aed58da75a5c6ab9c888896e5fb13ee08
Miska Rihu <miska.rihu@tuni.fi>
2026-04-25 18:38:06 +0300

    clean project by removing old versions

--- Code/Wall.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2026 Miska Rihu
-// License: MIT License (see LICENSE in project root)
-
-using Godot;
-
-namespace MathAndShaders
-{
-    public partial class Wall : MeshInstance3D { }
-}
